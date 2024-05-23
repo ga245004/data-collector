98e26d61c477b151c51c80f886eb7ecabae670b0
Gaurav Maheshwari <gmaheshwari3@dxc.com>
2024-05-23 16:57:57 +0530

    update edit field ui

--- src/app/components/edit-field/edit-field.component.ts
+++ src/app/components/edit-field/edit-field.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Section } from '../../models/section.model';
 import { SectionsActions } from '../../state/sections.actions';
 import { Store } from '@ngrx/store';
+import { getRandomGradient } from '../../utils/randomTwGradient';
 
 @Component({
   selector: 'app-edit-field',
@@ -25,15 +26,23 @@ export class EditFieldComponent implements OnInit {
   @Input()
   field: string
 
+  randomG = getRandomGradient({ intensity: "100" })
+
   constructor(private store: Store) { }
 
   ngOnInit() {
   }
 
-  hideEditField() {
+  saveEditField() {
     this.item[this.field + 'edit'] = false;
     console.log({ sectionIndex: this.sectionIndex, props: { ...this.section } })
     this.store.dispatch(SectionsActions.updateSection({ sectionIndex: this.sectionIndex, props: { ...this.section } }))
+  }
+
+  
+  hideEditField() {
+    this.item[this.field + 'edit'] = false;
+    console.log({ sectionIndex: this.sectionIndex, props: { ...this.section } })
   }
 }
 
